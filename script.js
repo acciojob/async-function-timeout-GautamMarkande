6233c6cae9f3d4e2441c60ec49e61eb48b35e7aa
GautamMarkande <118103083+GautamMarkande@users.noreply.github.com>
2023-07-19 10:46:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-//your JS code here. If required.
+//your JS code here. If require
+
+
+const btn = document.getElementById("btn");
+async function callme(){
+	let text = document.getElementById("text").value;
+    let delay = parseInt(document.getElementById("delay").value);
+	 text = await new Promise((resolve)=>{
+		setTimeout(()=>{
+			resolve(text);
+		},delay)
+	})
+return text;
+}
+btn.addEventListener("click", (e)=>{
+	const div = document.getElementById("output");
+	callme().then((data)=>{
+		div.innerText = data;
+	});
+});
